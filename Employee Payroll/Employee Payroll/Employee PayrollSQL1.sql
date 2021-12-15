0be48b949782d007fe1eb827f57f1f4d1b4761e1
keshavprakash98 <keshavprakash98@gmail.com>
2021-12-15 23:15:15 +0530

    EMPLOYEE-PAYROLL

---
+++ Employee Payroll/Employee Payroll/Employee PayrollSQL1.sql
@@ -0,0 +1,33 @@
+create database payroll_service;
+
+use payroll_service
+
+/*UC2*/
+create table empolye_payroll
+(
+  id      INT identity (1,1) primary key,
+  name    VARCHAR(150) NOT NULL,
+  salary  float Not NULL,
+  start   DATE NOT NULL,
+
+);
+exec sp_help employee_payroll;
+
+/*UC3*/
+insert into empolye_payroll(name,salary,start) values
+('Keshav' ,400000 ,'2021-01-15'),
+('ram' , 390000 ,'2021-02-15'),
+('shyam' ,380000 ,'2021-03-15');
+
+DESCRIBE employee_payroll;
+
+/*UC4*/
+select * from empolye_payroll;
+
+/*UC5*/
+select name ,salary from employee_payroll where name = Keshav;
+select * from employee_payroll
+where start between cast ('01-01-2021' as date) and get date();
+
+
+
